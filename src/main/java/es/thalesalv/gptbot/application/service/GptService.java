7b7b13082093893ef4b2e16e4c28c2d2573f127c
Thales Alves <thales@thalesalv.es>
2023-02-28 18:25:45 +0000

    organized files for other openai apis

--- src/main/java/es/thalesalv/gptbot/application/service/GptService.java
+++ src/main/java/es/thalesalv/gptbot/application/service/GptService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 
 import es.thalesalv.gptbot.adapters.rest.OpenAIApiService;
 import es.thalesalv.gptbot.application.translator.GptRequestTranslator;
-import es.thalesalv.gptbot.domain.model.gpt.GptRequestEntity;
-import es.thalesalv.gptbot.domain.model.gpt.GptResponseEntity;
+import es.thalesalv.gptbot.domain.model.openai.gpt.GptRequest;
+import es.thalesalv.gptbot.domain.model.openai.gpt.GptResponse;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -24,32 +24,32 @@ public class GptService {
     private static final String MODEL_DAVINCI = "text-davinci-003";
     private static final Logger LOGGER = LoggerFactory.getLogger(GptService.class);
 
-    public Mono<GptResponseEntity> callModel(final String prompt, final String model) {
+    public Mono<GptResponse> callModel(final String prompt, final String model) {
 
         LOGGER.debug("Sending prompt to model. Model -> {}, Prompt -> {}", model, prompt);
-        final GptRequestEntity request = gptRequestTranslator.buildRequest(prompt, model);
+        final GptRequest request = gptRequestTranslator.buildRequest(prompt, model);
         return openAiService.callGptApi(request);
     }
 
-    public Mono<GptResponseEntity> callDaVinci(final String prompt) {
+    public Mono<GptResponse> callDaVinci(final String prompt) {
 
         LOGGER.debug("Called inference with Davinci");
         return callModel(prompt, MODEL_DAVINCI);
     }
 
-    public Mono<GptResponseEntity> callAda(final String prompt) {
+    public Mono<GptResponse> callAda(final String prompt) {
 
         LOGGER.debug("Called inference with Ada");
         return callModel(prompt, MODEL_ADA);
     }
 
-    public Mono<GptResponseEntity> callCurie(final String prompt) {
+    public Mono<GptResponse> callCurie(final String prompt) {
 
         LOGGER.debug("Called inference with Curie");
         return callModel(prompt, MODEL_CURIE);
     }
 
-    public Mono<GptResponseEntity> callBabbage(final String prompt) {
+    public Mono<GptResponse> callBabbage(final String prompt) {
 
         LOGGER.debug("Called inference with Babbage");
         return callModel(prompt, MODEL_BABBAGE);
