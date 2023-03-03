556187d730d539165dfa23aa3f017e093381f891
Thales Alves <thales@thalesalv.es>
2023-03-03 02:24:18 +0000

    made argument nullable and final

--- src/main/java/es/thalesalv/gptbot/application/util/YamlPropertySourceFactory.java
+++ src/main/java/es/thalesalv/gptbot/application/util/YamlPropertySourceFactory.java
@@ -8,11 +8,12 @@ import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
 
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
     @Override
-    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) throws IOException {
+    public PropertySource<?> createPropertySource(@Nullable final String name, final EncodedResource encodedResource) throws IOException {
 
         final YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
         factory.setResources(encodedResource.getResource());
