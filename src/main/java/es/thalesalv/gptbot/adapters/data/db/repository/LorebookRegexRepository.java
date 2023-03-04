93cd2efa9d61f56dab37ee06b180c5940cb452ad
Thales Alves <thales@thalesalv.es>
2023-03-04 03:25:55 +0000

    implemented delete command

--- src/main/java/es/thalesalv/gptbot/adapters/data/db/repository/LorebookRegexRepository.java
+++ src/main/java/es/thalesalv/gptbot/adapters/data/db/repository/LorebookRegexRepository.java
@@ -6,10 +6,12 @@ import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import es.thalesalv.gptbot.adapters.data.db.entity.LorebookEntry;
 import es.thalesalv.gptbot.adapters.data.db.entity.LorebookRegex;
 
 @Repository
 public interface LorebookRegexRepository extends CrudRepository<LorebookRegex, UUID> {
 
     List<LorebookRegex> findAll();
+    void deleteByLorebookEntry(LorebookEntry lorebookEntry);
 }
