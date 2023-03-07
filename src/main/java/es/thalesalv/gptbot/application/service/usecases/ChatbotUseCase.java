76d013747932f8856ca7b752a72274cb0743845b
Shel <gryzon@gmail.com>
2023-03-07 15:50:20 -0500

    REFACTOR: ChatbotUseCase

--- src/main/java/es/thalesalv/gptbot/application/service/usecases/ChatbotUseCase.java
+++ src/main/java/es/thalesalv/gptbot/application/service/usecases/ChatbotUseCase.java
@@ -1,16 +1,5 @@
 package es.thalesalv.gptbot.application.service.usecases;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import es.thalesalv.gptbot.application.config.MessageEventData;
 import es.thalesalv.gptbot.application.config.Persona;
 import es.thalesalv.gptbot.application.service.ModerationService;
@@ -18,9 +7,19 @@ import es.thalesalv.gptbot.application.service.interfaces.GptModelService;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Mentions;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.SelfUser;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -30,79 +29,57 @@ public class ChatbotUseCase implements BotUseCase {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatbotUseCase.class);
 
+    private static String formatMessage(Message message, String botName) {
+        return MessageFormat.format("{0} said: {1}", message.getAuthor().getName(),
+                message.getContentDisplay().replaceAll("(@|)" + botName, StringUtils.EMPTY).trim());
+    }
+
+    private static String formatMessageWithReply(Message message) {
+        return MessageFormat.format("{0} said earlier: {1}\n{2} quoted the message from {3} and replied with: {4}",
+                Optional.ofNullable(message.getReferencedMessage()).map(Message::getAuthor).map(User::getName).orElse(""),
+                Optional.ofNullable(message.getReferencedMessage()).map(Message::getContentDisplay).orElse(""),
+                message.getAuthor().getName(),
+                Optional.ofNullable(message.getReferencedMessage()).map(Message::getAuthor).map(User::getName).orElse(""),
+                message.getContentDisplay());
+    }
+
     @Override
     public void generateResponse(final Persona persona, final MessageEventData messageEventData, final Mentions mentions, final GptModelService model) {
 
         LOGGER.debug("Entered generation for normal text.");
         messageEventData.getChannel().sendTyping().complete();
-        final List<String> messages = new ArrayList<>();
-        final Message replyMessage = messageEventData.getMessage().getReferencedMessage();
+        Message message = messageEventData.getMessage();
+        Message referenceMessage = message.getReferencedMessage();
+        MessageChannelUnion channel = messageEventData.getChannel();
+        int limit = persona.getChatHistoryMemory();
+        String botName = messageEventData.getBot().getName();
+        final List<String> messages = Optional.ofNullable(referenceMessage)
+                .map(r -> channel.getHistoryBefore(r, limit).complete().getRetrievedHistory())
+                .orElseGet(() -> channel.getHistory().retrievePast(limit).complete())
+                .stream().map(m ->
+                        null == m.getReferencedMessage() ?
+                                formatMessage(m, botName) :
+                                formatMessageWithReply(m)
+                )
+                .collect(Collectors.toList());
+        Collections.reverse(messages);
 
-        if (replyMessage != null) {
-            handleMessageHistoryForReplies(persona, messages, messageEventData.getMessage(),
-                    messageEventData.getBot(), messageEventData.getMessageAuthor(), messageEventData.getChannel());
-        } else {
-            handleMessageHistory(persona, messages, messageEventData.getBot(), messageEventData.getChannel());
+        if (null != referenceMessage) {
+            messages.addAll(Arrays.asList(
+                    MessageFormat.format("{0} said earlier: {1}",
+                            referenceMessage.getAuthor().getName(), referenceMessage.getContentDisplay()),
+                    MessageFormat.format("{0} quoted the message from {1} and replied with: {2}",
+                            message.getAuthor().getName(), referenceMessage.getAuthor().getName(), message.getContentDisplay())
+            ));
         }
 
         final String chatifiedMessage = chatifyMessages(messageEventData.getBot(), messages);
+
         moderationService.moderate(messageEventData, persona, chatifiedMessage)
                 .subscribe(moderationResult -> model.generate(messageEventData, chatifiedMessage, persona, messages)
-                .subscribe(textResponse -> messageEventData.getChannel().sendMessage(textResponse).queue()));
+                        .subscribe(textResponse -> messageEventData.getChannel().sendMessage(textResponse).queue()));
     }
 
-    /**
-     * Formats last messages history on replied to give the AI context on the past conversation
-     * @param persona Persona containing the current bot settings
-     * @param messages List of messages before the one replied to
-     * @param message Message sent as a reply to the quoted message
-     * @param replyMessage Quoted message
-     * @param bot Bot user
-     * @param messageAuthor User who wrote the reply
-     * @param channel Channel where the conversation is happening
-     */
-    private void handleMessageHistoryForReplies(final Persona persona, final List<String> messages,
-            final Message message, final SelfUser bot, final User messageAuthor, final MessageChannelUnion channel) {
-
-        LOGGER.debug("Entered quoted message history handling for chatbot");
-        Message reply = message.getReferencedMessage();
-        channel.getHistoryBefore(reply, persona.getChatHistoryMemory())
-                .complete()
-                .getRetrievedHistory()
-                .forEach(m -> {
-                    final User mAuthorUser = m.getAuthor();
-                    messages.add(MessageFormat.format("{0} said: {1}", mAuthorUser.getName(),
-                            m.getContentDisplay().replaceAll("(@|)" + bot.getName(), StringUtils.EMPTY).trim()));
-                });
-
-        Collections.reverse(messages);
-        messages.add(MessageFormat.format("{0} said earlier: {1}",
-                reply.getAuthor().getName(), reply.getContentDisplay()));
-
-        messages.add(MessageFormat.format("{0} quoted the message from {1} and replied with: {2}",
-                messageAuthor.getName(), reply.getAuthor().getName(), message.getContentDisplay()));
-    }
-
-    /**
-     * Formats last messages history to give the AI context on the current conversation
-     * @param persona Persona with the current bot settings
-     * @param messages List messages before the one sent
-     * @param bot Bot user
-     * @param channel Channel where the conversation is happening
-     */
-    private void handleMessageHistory(final Persona persona, final List<String> messages, final SelfUser bot, final MessageChannelUnion channel) {
-
-        LOGGER.debug("Entered message history handling for chatbot");
-        channel.getHistory()
-                .retrievePast(persona.getChatHistoryMemory())
-                .complete().forEach(m -> {
-                    final User mAuthorUser = m.getAuthor();
-                    messages.add(MessageFormat.format("{0} said: {1}", mAuthorUser.getName(),
-                            m.getContentDisplay().replaceAll("(@|)" + bot.getName(), StringUtils.EMPTY).trim()));
-                });;
-
-        Collections.reverse(messages);
-    }
 
     private static String chatifyMessages(final User bot, final List<String> messages) {
 
