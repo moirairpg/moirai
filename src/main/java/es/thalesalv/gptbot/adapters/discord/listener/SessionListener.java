7f86d9f15bd4fcdeb95508a02dfbb801898b4072
Shel <gryzon@gmail.com>
2023-03-09 10:42:44 -0500

    REFACTOR: Improved JDA event handling
    I created a single EventDispatcher which routes events to appropriate
    listeners segregated by JDA category. The is largely cosmetic as no
    functionality has been changed, however this is in anticipation of
    future functionality updates.

--- src/main/java/es/thalesalv/gptbot/adapters/discord/listener/SessionListener.java
+++ src/main/java/es/thalesalv/gptbot/adapters/discord/listener/SessionListener.java
@@ -1,32 +1,28 @@
 package es.thalesalv.gptbot.adapters.discord.listener;
 
+import net.dv8tion.jda.api.events.session.SessionDisconnectEvent;
+import net.dv8tion.jda.api.events.session.ShutdownEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.SelfUser;
-import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 @Service
-public class GenericEventListener implements EventListener {
+public class SessionListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GenericEventListener.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionListener.class);
 
-    @Override
-    public void onEvent(GenericEvent event) {
-
+    public void onReady(ReadyEvent event) {
         try {
             final SelfUser bot = event.getJDA().getSelfUser();
-            if (event instanceof ReadyEvent) {
                 LOGGER.info("{} is ready to chat!", bot.getName());
                 registerSlashCommands(event);
-            }
         } catch (IllegalStateException e) {
             if (e.getMessage().contains("Session is not yet ready!")) {
                 LOGGER.warn("Waiting for Discord session...");
@@ -36,7 +32,25 @@ public class GenericEventListener implements EventListener {
         }
     }
 
-    private void registerSlashCommands(GenericEvent event) {
+    public void onSessionDisconnect(SessionDisconnectEvent event) {
+        try {
+            final SelfUser bot = event.getJDA().getSelfUser();
+            LOGGER.info("{} is disconnected.", bot.getName());
+        } catch (Throwable t) {
+            LOGGER.error("Error during disconnect: ", t);
+        }
+    }
+
+    public void onShutdown(ShutdownEvent event) {
+        try {
+            final SelfUser bot = event.getJDA().getSelfUser();
+            LOGGER.info("{} is shutdown.", bot.getName());
+        } catch (Throwable t) {
+            LOGGER.error("Error during shutdown: ", t);
+        }
+    }
+
+    private void registerSlashCommands(ReadyEvent event) {
 
         LOGGER.debug("Registering slash commands.");
         final JDA jda = event.getJDA();
