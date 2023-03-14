3f9d2da0918ead5af1585714909ffc788f08b53d
Thales Alves <thales@thalesalv.es>
2023-03-14 11:40:31 +0100

    another desperate attempt to see if tokenizer works in the deploy server

--- src/main/java/es/thalesalv/chatrpg/application/service/tokenizer/TokenizerService.java
+++ src/main/java/es/thalesalv/chatrpg/application/service/tokenizer/TokenizerService.java
@@ -1,5 +1,15 @@
 package es.thalesalv.chatrpg.application.service.tokenizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Comparator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer;
@@ -7,11 +17,13 @@ import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer;
 @Component
 public class TokenizerService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenizerService.class);
+
     private final HuggingFaceTokenizer tokenizer;
 
     public TokenizerService() {
         try {
-            tokenizer = HuggingFaceTokenizer.newInstance("tokenizer.json");
+            tokenizer = HuggingFaceTokenizer.newInstance(getFileFromJar("tokenizer.json"));
         } catch (Exception e) {
             throw new RuntimeException("Failed to initialize tokenizer", e);
         }
@@ -32,5 +44,25 @@ public class TokenizerService {
 
     public int countTokens(String[] texts) {
         return toTokenIds(texts).length;
+    }
+
+    public Path getFileFromJar(String filePathInJar) throws IOException {
+        
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePathInJar);
+        Path tempDirPath = Files.createTempDirectory("temp-dir");
+        Path tempFilePath = Files.write(tempDirPath.resolve("temp-file"), inputStream.readAllBytes(), StandardOpenOption.CREATE);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                Files.walk(tempDirPath)
+                     .sorted(Comparator.reverseOrder())
+                     .map(Path::toFile)
+                     .forEach(File::delete);
+            } catch (IOException e) {
+                LOGGER.error("Error retrieving path of tokenizer file", e);
+                throw new RuntimeException("Error retrieving path of tokenizer file", e);
+            }
+        }));
+
+        return tempFilePath;
     }
 }
