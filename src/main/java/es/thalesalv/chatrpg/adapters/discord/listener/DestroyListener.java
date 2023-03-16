94b88fccdb0ab29b80f7f2d0ac9189fd5ca8520e
Shel <gryzon@gmail.com>
2023-03-16 09:29:41 -0400

    FIX: Made status channel optional on destroy

--- src/main/java/es/thalesalv/chatrpg/adapters/discord/listener/DestroyListener.java
+++ src/main/java/es/thalesalv/chatrpg/adapters/discord/listener/DestroyListener.java
@@ -5,10 +5,12 @@ import es.thalesalv.chatrpg.application.config.BotConfig;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class DestroyListener {
@@ -19,7 +21,11 @@ public class DestroyListener {
     @PreDestroy
     public void beforeDestroy() {
         try {
-            jdaConfig.jda().getChannelById(TextChannel.class, botConfig.getStatusChannelId()).sendMessage(jdaConfig.jda().getSelfUser().getName() + " is ready to chat!").complete();
+            Optional.ofNullable(botConfig.getStatusChannelId())
+                    .filter(StringUtils::isNotEmpty)
+                    .ifPresent(
+                            id -> jdaConfig.jda().getChannelById(TextChannel.class, id).sendMessage(jdaConfig.jda().getSelfUser().getName() + " is ready to chat!").complete()
+                    );
         } catch (Exception e) {
             LOGGER.error("Error during destroy: ", e);
         }
