5784b07d33db542d749b6d14b09d969ba38c3832
Thales Alves <thales@thalesalv.es>
2023-03-22 00:10:05 +0000

    fixed bug with wrong name replace and role, as well as a bug with an immutable array list

--- src/main/java/es/thalesalv/chatrpg/application/service/completion/ChatCompletionService.java
+++ src/main/java/es/thalesalv/chatrpg/application/service/completion/ChatCompletionService.java
@@ -1,5 +1,6 @@
 package es.thalesalv.chatrpg.application.service.completion;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -44,20 +45,21 @@ public class ChatCompletionService implements CompletionService {
         LOGGER.debug("Called inference for Chat Completions.");
         final Mentions mentions = eventData.getMessage().getMentions();
         final User author = eventData.getMessageAuthor();
+        final List<String> processedMessages = new ArrayList<>(messages);
         final Set<LorebookEntryEntity> entriesFound = new HashSet<>();
         final Persona persona = eventData.getChannelConfig().getPersona();
         outputProcessor.addRule(s -> Pattern.compile("\\bAs " + persona.getName() + ", (\\w)").matcher(s).replaceAll(r -> r.group(1).toUpperCase()));
         outputProcessor.addRule(s -> Pattern.compile("\\bas " + persona.getName() + ", (\\w)").matcher(s).replaceAll(r -> r.group(1)));
 
-        messageFormatHelper.handleEntriesMentioned(messages, entriesFound);
+        messageFormatHelper.handleEntriesMentioned(processedMessages, entriesFound);
         if (persona.getIntent().equals("dungeonMaster")) {
-            messageFormatHelper.handlePlayerCharacterEntries(entriesFound, messages, author, mentions);
-            messageFormatHelper.processEntriesFoundForRpg(entriesFound, messages, author.getJDA());
+            messageFormatHelper.handlePlayerCharacterEntries(entriesFound, processedMessages, author, mentions);
+            messageFormatHelper.processEntriesFoundForRpg(entriesFound, processedMessages, author.getJDA());
         } else {
-            messageFormatHelper.processEntriesFoundForChat(entriesFound, messages);
+            messageFormatHelper.processEntriesFoundForChat(entriesFound, processedMessages);
         }
 
-        final List<ChatMessage> chatMessages = messageFormatHelper.formatMessagesForChatCompletions(messages, eventData);
+        final List<ChatMessage> chatMessages = messageFormatHelper.formatMessagesForChatCompletions(processedMessages, eventData, eventData.getBot());
         final ChatCompletionRequest request = chatCompletionsRequestTranslator.buildRequest(chatMessages, eventData.getChannelConfig());
         return openAiService.callGptChatApi(request, eventData)
                 .map(response -> {
