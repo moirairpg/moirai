1768dee5956a8b8edee4b9185e219fd7c4d142cc
Shel <gryzon@gmail.com>
2023-03-23 07:38:09 -0400

    TEST: StringProcessorTest

---
+++ src/test/java/es/thalesalv/chatrpg/application/util/StringProcessorTest.java
@@ -0,0 +1,20 @@
+package es.thalesalv.chatrpg.application.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringProcessorTest {
+    String toTest = "Shel said: [ The man draws his sword. ]";
+
+    @Test
+    public void test() {
+        StringProcessor processor = new StringProcessor();
+        processor.addRule(s -> Pattern.compile("Selkie").matcher(s).replaceAll(r -> "Janet"));
+
+        assertEquals(toTest, processor.process(toTest));
+    }
+
+}
