b7bb8722dbbde9b3fded8e2c1337f8661b70a326
MagicShel <81432309+MagicShel@users.noreply.github.com>
2023-03-23 08:06:37 -0400

    Fix/completion memory leak (#93)
    * FIX: StringProcessor memory leak
    
    * FIX: StringProcessor memory leak

--- src/main/java/es/thalesalv/chatrpg/application/service/completion/TextCompletionService.java
+++ src/main/java/es/thalesalv/chatrpg/application/service/completion/TextCompletionService.java
@@ -32,8 +32,6 @@ public class TextCompletionService implements CompletionService {
     private final CommonErrorHandler commonErrorHandler;
     private final TextCompletionRequestTranslator textCompletionRequestTranslator;
     private final OpenAIApiService openAiService;
-    private final StringProcessor outputProcessor;
-    private final StringProcessor inputProcessor;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TextCompletionService.class);
 
@@ -41,6 +39,8 @@ public class TextCompletionService implements CompletionService {
     public Mono<String> generate(List<String> messages, MessageEventData eventData) {
 
         LOGGER.debug("Called inference for Text Completions.");
+        final StringProcessor outputProcessor = new StringProcessor();
+        final StringProcessor inputProcessor = new StringProcessor();
         final Mentions mentions = eventData.getMessage().getMentions();
         final User author = eventData.getMessageAuthor();
         final Persona persona = eventData.getChannelConfig().getPersona();
