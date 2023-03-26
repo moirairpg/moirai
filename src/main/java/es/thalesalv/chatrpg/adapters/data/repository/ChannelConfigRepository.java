d9df35054330b246d8c198486316566289903ee8
Thales Alves <thales@thalesalv.es>
2023-03-26 02:34:32 +0100

    removed unnecessary annotation

--- src/main/java/es/thalesalv/chatrpg/adapters/data/repository/ChannelConfigRepository.java
+++ src/main/java/es/thalesalv/chatrpg/adapters/data/repository/ChannelConfigRepository.java
@@ -1,11 +1,9 @@
 package es.thalesalv.chatrpg.adapters.data.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import es.thalesalv.chatrpg.adapters.data.entity.ChannelConfigEntity;
 
-@Repository
 public interface ChannelConfigRepository extends CrudRepository<ChannelConfigEntity, String> {
 
 }
--- src/main/java/es/thalesalv/chatrpg/adapters/data/repository/LorebookRegexRepository.java
+++ src/main/java/es/thalesalv/chatrpg/adapters/data/repository/LorebookRegexRepository.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import es.thalesalv.chatrpg.adapters.data.entity.LorebookEntryEntity;
 import es.thalesalv.chatrpg.adapters.data.entity.LorebookEntryRegexEntity;
 
-@Repository
 public interface LorebookRegexRepository extends CrudRepository<LorebookEntryRegexEntity, String> {
 
     List<LorebookEntryRegexEntity> findAll();
