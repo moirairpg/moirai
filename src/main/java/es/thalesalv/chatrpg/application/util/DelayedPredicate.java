9656a078c5bf5c94cfe75a1e0308d08702338b7e
Shel <gryzon@gmail.com>
2023-03-27 08:45:32 -0400

    FEAT: DelayedPredicate

---
+++ src/main/java/es/thalesalv/chatrpg/application/util/DelayedPredicate.java
@@ -0,0 +1,19 @@
+package es.thalesalv.chatrpg.application.util;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
+
+public class DelayedPredicate<T> implements Predicate<T> {
+    private final AtomicBoolean override = new AtomicBoolean(false);
+    private final Predicate<T> test;
+    private DelayedPredicate(Predicate<T> test) {
+        this.test = test;
+    }
+    public static <T> Predicate<T> withTest(Predicate<T> test) {
+        return new DelayedPredicate<>(test);
+    }
+    @Override
+    public boolean test(T t) {
+        return override.getAndSet(test.test(t));
+    }
+}
