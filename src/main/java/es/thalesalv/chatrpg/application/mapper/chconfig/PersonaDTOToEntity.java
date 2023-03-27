70390bca2cdd9fb1992b965b436e7989bd9dbbeb
Thales Alves <thales@thalesalv.es>
2023-03-27 02:00:15 +0100

    added null validations to bump and nudge on persona

--- src/main/java/es/thalesalv/chatrpg/application/mapper/chconfig/PersonaDTOToEntity.java
+++ src/main/java/es/thalesalv/chatrpg/application/mapper/chconfig/PersonaDTOToEntity.java
@@ -1,12 +1,16 @@
 package es.thalesalv.chatrpg.application.mapper.chconfig;
 
+import java.util.Optional;
 import java.util.function.Function;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import es.thalesalv.chatrpg.adapters.data.entity.BumpEntity;
 import es.thalesalv.chatrpg.adapters.data.entity.NudgeEntity;
 import es.thalesalv.chatrpg.adapters.data.entity.PersonaEntity;
+import es.thalesalv.chatrpg.domain.model.chconf.Bump;
+import es.thalesalv.chatrpg.domain.model.chconf.Nudge;
 import es.thalesalv.chatrpg.domain.model.chconf.Persona;
 
 @Component
@@ -21,15 +25,36 @@ public class PersonaDTOToEntity implements Function<Persona, PersonaEntity> {
                 .owner(t.getOwner())
                 .intent(t.getIntent())
                 .personality(t.getPersonality())
-                .bump(BumpEntity.builder()
-                        .role(t.getBump().getRole())
-                        .content(t.getBump().getContent())
-                        .frequency(t.getBump().getFrequency())
-                        .build())
-                .nudge(NudgeEntity.builder()
-                        .role(t.getNudge().getRole())
-                        .content(t.getNudge().getContent())
-                        .build())
+                .bump(buildBump(t.getBump()))
+                .nudge(buildNudge(t.getNudge()))
                 .build();
+    }
+
+    private NudgeEntity buildNudge(Nudge nudge) {
+
+        return Optional.ofNullable(nudge)
+                .map(n -> NudgeEntity.builder()
+                        .role(n.getRole())
+                        .content(n.getContent())
+                        .build())
+                .orElse(NudgeEntity.builder()
+                        .role(StringUtils.EMPTY)
+                        .content(StringUtils.EMPTY)
+                        .build());
+    }
+
+    private BumpEntity buildBump(Bump bump) {
+
+        return Optional.ofNullable(bump)
+                .map(b -> BumpEntity.builder()
+                        .role(b.getRole())
+                        .content(b.getContent())
+                        .frequency(b.getFrequency())
+                        .build())
+                .orElse(BumpEntity.builder()
+                        .content(StringUtils.EMPTY)
+                        .role(StringUtils.EMPTY)
+                        .frequency(0)
+                        .build());
     }
 }
