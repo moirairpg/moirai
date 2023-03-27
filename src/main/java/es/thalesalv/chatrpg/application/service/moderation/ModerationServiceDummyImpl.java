ab47d81f7916df268d0a08130468ee582bfdeb68
Thales Alves <thales@thalesalv.es>
2023-03-27 19:30:33 +0100

    added profile to yaml; added logs to dummy moderation

--- src/main/java/es/thalesalv/chatrpg/application/service/moderation/ModerationServiceDummyImpl.java
+++ src/main/java/es/thalesalv/chatrpg/application/service/moderation/ModerationServiceDummyImpl.java
@@ -2,6 +2,8 @@ package es.thalesalv.chatrpg.application.service.moderation;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +16,26 @@ import reactor.core.publisher.Mono;
 @Profile("no-moderation")
 public class ModerationServiceDummyImpl implements ModerationService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModerationServiceDummyImpl.class);
+
     @Override
     public Mono<ModerationResponse> moderate(String content, EventData eventData, ModalInteractionEvent event) {
 
+        LOGGER.info("Debug mode enabled. Skipping moderation.");
         return Mono.just(ModerationResponse.builder().build());
     }
 
     @Override
     public Mono<ModerationResponse> moderate(List<String> messages, EventData eventData) {
 
+        LOGGER.info("Debug mode enabled. Skipping moderation.");
         return Mono.just(ModerationResponse.builder().build());
     }
 
     @Override
     public Mono<ModerationResponse> moderateOutput(String output, EventData eventData) {
 
+        LOGGER.info("Debug mode enabled. Skipping moderation.");
         return Mono.just(ModerationResponse.builder().build());
     }
 }
