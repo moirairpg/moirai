9c5bdd6a42ac646a79c4872860a4a28d25d4da0b
Shel <gryzon@gmail.com>
2023-03-28 09:02:14 -0400

    REFACTOR: exception throwing
    Replaced some new FooException() w/ FooException::new

--- src/main/java/es/thalesalv/chatrpg/application/service/commands/RetryCommandService.java
+++ src/main/java/es/thalesalv/chatrpg/application/service/commands/RetryCommandService.java
@@ -75,7 +75,7 @@ public class RetryCommandService implements DiscordCommand {
 
                         return ch;
                     })
-                    .orElseThrow(() -> new ChannelConfigNotFoundException());
+                    .orElseThrow(ChannelConfigNotFoundException::new);
         } catch (ChannelConfigNotFoundException e) {
             LOGGER.debug(NO_CONFIG_ATTACHED);
             event.reply(NO_CONFIG_ATTACHED).setEphemeral(true)
