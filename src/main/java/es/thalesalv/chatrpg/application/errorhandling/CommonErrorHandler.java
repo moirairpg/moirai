8e8b695530f80505df40c5664211de897d385715
Thales Alves <thales@thalesalv.es>
2023-05-09 00:02:55 +0100

    implemented more error handling for api

--- src/main/java/es/thalesalv/chatrpg/application/errorhandling/CommonErrorHandler.java
+++ src/main/java/es/thalesalv/chatrpg/application/errorhandling/CommonErrorHandler.java
@@ -18,9 +18,9 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class CommonErrorHandler {
 
-    private static final String EMPTY_RESPONSE = "The model did not generate an output due to a problem. Please try again. Your message will be removed.\n**Message content:** {0}";
-    private static final String UNKNOWN_ERROR = "An error occured and the message could not be sent to the model. Your message will be removed. Please try again.\n**Message content:** {0}";
-    private static final String MESSAGE_TOO_LONG = "The message you sent or the total context exceeds the maximum number of available tokens. Send a smaller message or contact the administrator. Your message will be removed.\n**Message content:** {0}";
+    private static final String EMPTY_RESPONSE = "The model did not generate an output due to a problem. Please try again.\n**Message content:** {0}";
+    private static final String UNKNOWN_ERROR = "An error occured and the message could not be sent to the model. Please try again.\n**Message content:** {0}";
+    private static final String MESSAGE_TOO_LONG = "The message you sent or the total context contained in this channel exceeds the maximum number of available tokens. Send a smaller message or contact the administrator.";
     private static final String SOMETHING_WRONG = "Something went wrong. Please try again.";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommonErrorHandler.class);
@@ -46,12 +46,40 @@ public class CommonErrorHandler {
                 .map(errorResponse -> {
                     if (404 == clientResponse.statusCode()
                             .value()) {
-                        LOGGER.error("OpenAI API threw NOT FOUND error -> {}", errorResponse);
+                        LOGGER.error("OpenAI API threw NOT FOUND error -> {}", errorResponse.getError()
+                                .getMessage());
+
                         notifyUser(SOMETHING_WRONG, eventData);
+                    } else if (429 == clientResponse.statusCode()
+                            .value()) {
+                        LOGGER.error("OpenAI API is currently overloaded or quota limit has been reached -> {}",
+                                errorResponse.getError()
+                                        .getMessage());
+
+                        notifyUser(SOMETHING_WRONG, eventData);
+                    } else if (400 == clientResponse.statusCode()
+                            .value()) {
+
+                        String errorMessage = errorResponse.getError()
+                                .getMessage();
+
+                        if (errorMessage.contains("This model's maximum context length")) {
+                            LOGGER.error("Maximum token context for model surpassed -> {}", errorResponse.getError()
+                                    .getMessage());
+
+                            notifyUser(MESSAGE_TOO_LONG, eventData);
+                        } else {
+                            LOGGER.error("There was a problem calling OpenAI API. Server return BAD REQUEST -> {}",
+                                    errorResponse.getError()
+                                            .getMessage());
+
+                            notifyUser(SOMETHING_WRONG, eventData);
+                        }
                     } else {
                         LOGGER.error("Error while calling OpenAI API. Message -> {}", errorResponse.getError()
                                 .getMessage());
-                        notifyUser(MESSAGE_TOO_LONG, eventData);
+
+                        notifyUser(SOMETHING_WRONG, eventData);
                     }
                     return new OpenAiApiException("Error while calling OpenAI API.", errorResponse);
                 });
