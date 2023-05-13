8179ccb4694976b6a83b3dc1b641a28551850a8d
Thales Alves <thales@thalesalv.es>
2023-05-14 00:16:16 +0100

    fix process of null values for non-required fields of channel conf; fixed escape of new line for default config

--- src/main/java/es/thalesalv/chatrpg/application/util/dbutils/StringMapDoubleConverter.java
+++ src/main/java/es/thalesalv/chatrpg/application/util/dbutils/StringMapDoubleConverter.java
@@ -1,6 +1,9 @@
 package es.thalesalv.chatrpg.application.util.dbutils;
 
 import java.util.Map;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,20 +18,30 @@ public class StringMapDoubleConverter implements AttributeConverter<Map<String, 
     @Override
     public String convertToDatabaseColumn(Map<String, Double> attribute) {
 
-        try {
-            return new ObjectMapper().writeValueAsString(attribute);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error parsing string from map");
-        }
+        return Optional.ofNullable(attribute)
+                .filter(a -> !a.isEmpty())
+                .map(a -> {
+                    try {
+                        return new ObjectMapper().writeValueAsString(attribute);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Error parsing string from map");
+                    }
+                })
+                .orElse(null);
     }
 
     @Override
     public Map<String, Double> convertToEntityAttribute(String dbData) {
 
-        try {
-            return new ObjectMapper().readValue(dbData, Map.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error parsing map from string");
-        }
+        return Optional.ofNullable(dbData)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> {
+                    try {
+                        return new ObjectMapper().readValue(dbData, Map.class);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Error parsing map from string");
+                    }
+                })
+                .orElse(null);
     }
 }
