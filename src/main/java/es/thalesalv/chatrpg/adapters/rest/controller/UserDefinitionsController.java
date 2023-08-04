f728621b6eb0c8c2d78ee85c73b5bff3fdd29179
Thales Alves <thales@thalesalv.es>
2023-08-04 23:10:55 +0100

    added retrieval unit test

--- src/main/java/es/thalesalv/chatrpg/adapters/rest/controller/UserDefinitionsController.java
+++ src/main/java/es/thalesalv/chatrpg/adapters/rest/controller/UserDefinitionsController.java
@@ -2,6 +2,7 @@ package es.thalesalv.chatrpg.adapters.rest.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.thalesalv.chatrpg.application.service.UserDefinitionsService;
+import es.thalesalv.chatrpg.domain.exception.NotFoundException;
 import es.thalesalv.chatrpg.domain.model.bot.UserDefinitions;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -27,12 +29,23 @@ public class UserDefinitionsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDefinitionsController.class);
 
     @GetMapping
-    public Mono<ResponseEntity<UserDefinitions>> retrieveUserDefinitions(
-            @RequestHeader("requester") final String requesterUserId) {
+    public Mono<ResponseEntity<?>> retrieveUserDefinitions(@RequestHeader("requester") final String requesterUserId) {
 
-        LOGGER.info("Received request to retrieve user definitions for user with ID -> {}", requesterUserId);
-        final UserDefinitions userDefinitions = userDefinitionsService.retrieveUserDefinitions(requesterUserId);
-        return Mono.just(buildResponse(userDefinitions));
+        try {
+            LOGGER.info("Received request to retrieve user definitions for user with ID -> {}", requesterUserId);
+            final UserDefinitions userDefinitions = userDefinitionsService.retrieveUserDefinitions(requesterUserId);
+            return Mono.just(buildResponse(userDefinitions));
+        } catch (NotFoundException e) {
+            LOGGER.error("The requested user definition was not found", e);
+            return Mono.just(ResponseEntity.status(HttpStatus.FORBIDDEN.value())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("The requested user definition was not found"));
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving user definition", e);
+            return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("Something went wrong while retrieving user definition"));
+        }
     }
 
     @PutMapping("{user-id}")
