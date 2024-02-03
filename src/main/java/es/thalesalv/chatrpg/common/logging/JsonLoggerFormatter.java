84a38d14efe67fca1613cb06a2a08752d90133c8
Thales Alves <thales@thalesalv.es>
2024-02-03 00:52:00 +0000

    fixed logging

---
+++ src/main/java/es/thalesalv/chatrpg/common/logging/JsonLoggerFormatter.java
@@ -0,0 +1,21 @@
+package es.thalesalv.chatrpg.common.logging;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.contrib.json.classic.JsonLayout;
+
+public class JsonLoggerFormatter extends JsonLayout {
+
+    @Override
+    protected void addCustomDataToJsonMap(Map<String, Object> map, ILoggingEvent event) {
+
+        Stream.of(event.getArgumentArray())
+                .filter(arg -> !(arg instanceof String))
+                .filter(arg -> !(arg instanceof Number))
+                .filter(arg -> !(arg instanceof Boolean))
+                .filter(arg -> !(arg instanceof Character))
+                .forEach(arg -> map.put(arg.getClass().getSimpleName(), arg));
+    }
+}
