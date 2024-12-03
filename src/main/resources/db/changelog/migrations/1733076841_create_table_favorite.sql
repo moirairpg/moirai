d33ce548b5c07baaf69fc0600a9c147d0796dfa6
Thales Alves <thales@thalesalv.es>
2024-12-03 00:21:11 +0000

    fixed favorite table primary key

--- src/main/resources/db/changelog/migrations/1733076841_create_table_favorite.sql
+++ src/main/resources/db/changelog/migrations/1733076841_create_table_favorite.sql
@@ -3,10 +3,10 @@
 --preconditions onFail:HALT, onError:HALT
 
 CREATE TABLE favorite (
+    id VARCHAR(100) PRIMARY KEY,
     player_discord_id VARCHAR(100) NOT NULL,
     asset_id VARCHAR(100) NOT NULL,
-    asset_type VARCHAR(20) NOT NULL,
-    PRIMARY KEY(player_discord_id, asset_id, asset_type)
+    asset_type VARCHAR(20) NOT NULL
 );
 
 --rollback DROP TABLE favorite
