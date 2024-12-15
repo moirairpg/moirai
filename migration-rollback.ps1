718d4868e891c6e1722ef890a1682b857138f785
Thales Alves <thales@thalesalv.es>
2024-12-15 01:07:08 +0000

    added message to migration rollback

--- migration-rollback.ps1
+++ migration-rollback.ps1
@@ -2,4 +2,8 @@ param (
     [string]$amount
 )
 
+echo "Executing rollback for the last ${amount} migrations..."
+
 mvn liquibase:rollback `-Dliquibase.rollbackCount=${amount} `-Dliquibase.password=$env:POSTGRES_PASSWORD `-Dliquibase.url=jdbc:postgresql://$env:POSTGRES_HOST/$env:POSTGRES_DB `-Dliquibase.username=$env:POSTGRES_USER `-Dliquibase.driver="org.postgresql.Driver"
+
+echo "The last ${amount} migrations have been rolled back."
